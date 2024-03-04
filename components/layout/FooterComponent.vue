a06e7d979a8f63a4eca6014187cf755ab77b52f3
Branislav Hozza <brankohozza@gmail.com>
2024-03-04 10:58:43 +0100

    :sparkles: prepare layout

--- components/layout/FooterComponent.vue
+++ components/layout/FooterComponent.vue
@@ -1,3 +1,3 @@
 <template>
-  <div>Footer</div>
+  <div class="w-full flex justify-center items-center bg-slate-600 p-4 text-white">Footer</div>
 </template>
---
+++ components/layout/LeftSideContent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="h-full border-r my-2 border-gray-400 p-2">
+    <ul class="flex flex-col gap-2">
+      <li class="flex gap-1 text-xl items-center">
+        <icon size="24" name="mdi:home-outline" />
+        <span>Home</span>
+      </li>
+      <li class="flex gap-1 text-xl items-center">
+        <icon size="24" name="mdi:graph-line" />
+        <span>Trending</span>
+      </li>
+      <li class="flex gap-1 text-xl items-center">
+        <icon size="24" name="mdi:cog-outline" />
+        <span>Settings</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
