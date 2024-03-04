2fbdd4b5017a23ce786ed8b6f12402ef8a7a08b1
Branislav Hozza <brankohozza@gmail.com>
2024-03-04 10:58:03 +0100

    :sparkles: prepare some controls

---
+++ components/controls/TextInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <label v-if="label" class="underline font-bold" for="message">{{ label }}</label>
+  <input
+    v-model="model"
+    :readonly="readonly"
+    class="border-2 rounded-md p-2"
+    type="text"
+    name="message"
+    :placeholder="placeholder ?? undefined"
+  />
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  label: {
+    type: String as PropType<string | null>,
+    default: null,
+  },
+  placeholder: {
+    type: String as PropType<string | null>,
+    default: null,
+  },
+  readonly: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const model = defineModel<string | null>({
+  required: true,
+});
+</script>
+
+<style></style>
