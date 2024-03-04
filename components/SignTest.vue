8813e0a1cfba0920959bdc66c6c3464dc811b17f
Branislav Hozza <brankohozza@gmail.com>
2024-03-04 10:59:39 +0100

    :sparkles: dummy components

---
+++ components/SignTest.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <TextInput v-model="message" label="Original message:" placeholder="Here place your message" />
+    <TextInput v-model="signature" readonly label="Signature:" placeholder="Your signed message" />
+    <ConfirmButton text="Sign" @click="signMessage" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import TextInput from './controls/TextInput.vue';
+import ConfirmButton from './controls/ConfirmButton.vue';
+import { useAccountStore } from '~/store';
+
+const { $web3 } = useNuxtApp();
+const accountStore = useAccountStore();
+
+const account = computed(() => accountStore.account);
+const message = ref('');
+const signature = ref('');
+
+const signMessage = async () => {
+  if (window.ethereum && account.value) {
+    try {
+      const result = await $web3?.eth.personal.sign($web3.utils.utf8ToHex(message.value), account.value, '');
+      signature.value = result?.toString() ?? '';
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+</script>
+
+<style></style>
