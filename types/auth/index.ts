a3ccea3b3e7fa796c0cfb4ac7854c915e93056f4
Branislav Hozza <brankohozza@gmail.com>
2024-03-24 18:03:08 +0100

    :sparkles: prepare dtos

---
+++ types/auth/index.ts
@@ -0,0 +1,21 @@
+export interface AuthenticatedUser {
+  jwt: string;
+}
+
+export interface GetNonceDTO {
+  address: string;
+}
+
+export interface VerifyNonce {
+  signature: string;
+  address: string;
+}
+
+export interface RefreshTokenDTO {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export interface RefreshTokenRequestDTO {
+  refreshToken: string;
+}
