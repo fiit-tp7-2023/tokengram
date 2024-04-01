0784cc24b116fca46ca01de238634f109fe6a198
Karolína Tóthová <karolinka.tothova@gmail.com>
2024-04-01 17:46:03 +0200

    :recycle: rewrite endpoint for profile update

---
+++ types/dtos/user.ts
@@ -0,0 +1,10 @@
+export interface UserUpdateDTO {
+  Username: string;
+  ProfilePicture: string;
+}
+
+export interface UserResponseDTO {
+  address: string;
+  username: string;
+  profilePicture: string;
+}
--- types/user.ts
+++
@@ -1,7 +0,0 @@
-export interface UpdateUserProfilePictureDTO {
-  picturePath: string;
-}
-
-export interface UpdateUsernameDTO {
-  username: string;
-}
