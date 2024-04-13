cdd48ebb3e31a31c96665df99dc640866b7362f9
Branislav Hozza <brankohozza@gmail.com>
2024-04-13 15:14:38 +0200

    :sparkles: redesign search bar

--- components/controls/SearchBar.vue
+++ components/controls/SearchBar.vue
@@ -1,12 +1,22 @@
 <template>
-  <div
-    class="hidden md:flex items-center gap-2 relative justify-center w-full px-2 h-10 border-2 border-white rounded-md bg-white text-gray-400"
-  >
+  <div class="wrapper">
     <Icon name="mdi:search" size="24" />
-    <input
-      class="bg-transparent outline-none w-full h-full text-black placeholder:text-gray-400"
-      placeholder="Search"
-      type="text"
-    />
+    <input class="address-input" placeholder="Search" type="text" />
   </div>
 </template>
+<style scoped>
+.wrapper {
+  @apply flex items-center gap-2 relative justify-center w-full px-2 h-10 border-2 border-pink-300 rounded-md bg-transparent text-pink-300;
+
+  &:focus-within {
+    @apply border-pink-500 text-pink-500;
+  }
+}
+.address-input {
+  @apply bg-transparent outline-none w-full h-full text-white placeholder:text-pink-100;
+
+  &:focus {
+    @apply placeholder:invisible;
+  }
+}
+</style>
