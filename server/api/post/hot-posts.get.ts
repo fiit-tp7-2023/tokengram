12adc3d12afc32d6b8884bc17110ed26675d2b84
Adam Prisenznak <adam.prisen@gmail.com>
2024-04-20 23:53:15 +0200

    :adhesive_bandage: fix get jwt for hot posts

--- server/api/post/hot-posts.get.ts
+++ server/api/post/hot-posts.get.ts
@@ -1,8 +1,16 @@
 import { usePostService } from '~/server/services/post.service';
-import type { AuthenticatedUser, PaginationDTO } from '~/types/dtos';
+import type { PaginationDTO } from '~/types/dtos';
+import { useLogger } from '~/server/utils/logger';
 
 export default defineEventHandler(async (event) => {
-  const { jwt } = await readBody<AuthenticatedUser>(event);
+  const jwt = getHeader(event, 'Authorization')?.split('Bearer ')[1];
+  const logger = useLogger('POSTS::GET_HOT_POSTS');
+  if (!jwt) {
+    logger.error('Unauthorized');
+    throw createError({
+      message: 'Unauthorized',
+    });
+  }
   const { pageNumber, pageSize } = getQuery<PaginationDTO>(event);
 
   const service = usePostService(jwt);
